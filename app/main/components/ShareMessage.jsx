8ce2409ed9f5cbe9a1d9a41c1db29f9da4cd7a60
Kitae Kim <kkt920@gmail.com>
2024-09-02 02:41:06 +0900

    Revert "Relocate gallery and location components"
    This reverts commit 156bc50f7317a7cb2346471594e90e4d50db83be.

--- app/main/components/ShareMessage.jsx
+++ app/main/components/ShareMessage.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from "next/image";
-import Script from "next/script";
+// import Script from "next/script";
 import { useSearchParams } from "next/navigation"
 
 import React, { useState, useMemo } from "react";
@@ -24,6 +24,7 @@ const ShareMessage = () => {
     return (<></>);
   }
 
+  /*
   const handleKakaoTalkShare = () => {
     Kakao.Share.createDefaultButton({
       container: '#kakaotalk-sharing-btn',
@@ -36,10 +37,11 @@ const ShareMessage = () => {
       },
     });
   };
+  */
 
   return (
     <>
-      {
+      {/*
         (shareButtons.findIndex(item => item === "kakaotalk") > -1 ) &&
         <Script
           src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
@@ -50,15 +52,16 @@ const ShareMessage = () => {
             Kakao.init("b26e186abb61e7b9ce1010719b5d31e7");
           }}
         />
-      }
+      */}
+
       <section className="share-message w-full flex flex-col justify-center items-center py-[30px] bg-[#F5EEE1]">
-        {
+        {/*
           (shareButtons.findIndex(item => item === "kakaotalk") > -1 ) &&
           <div className="share-by-kakaotalk flex flex-row text-[15px] leading-[19px] mb-4 font-S-Core_Dream">
             <Image src={KakaoTalk} alt="카카오톡" width={24} height={24} />
             <button id="kakaotalk-sharing-btn" className="ml-2" onClick={handleKakaoTalkShare}>카카오톡 공유하기</button>
           </div>
-        }
+        */}
         {
           (shareButtons.findIndex(item => item === "link") > -1 ) &&
           <div className="share-by-link flex flex-row text-[15px] leading-[19px] mb-4 font-S-Core_Dream">
