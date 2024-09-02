e6f6aff83d0253d4bb716a7f4a444288e06e294c
Kitae Kim <kkt920@gmail.com>
2024-09-02 22:07:20 +0900

    add an image of kakao map

--- app/main/components/Location.jsx
+++ app/main/components/Location.jsx
@@ -1,4 +1,9 @@
+import Image from "next/image";
+
 import KakaoMap from "./KakaoMap";
+import KakaoMapImage from "../../../public/images/kakaomap_static_image.jpg";
+import KakaoMapIcon from "../../../public/images/kakao_map_icon.webp";
+import NaverMapIcon from "../../../public/images/naver_map_icon.webp";
 
 const Location = () => {
   return (
@@ -8,8 +13,19 @@ const Location = () => {
           오시는 길
         </div>
       </section>
-      <section className="location-map w-full flex px-5">
-        <KakaoMap />
+      <section className="location-map w-full flex flex-col px-5">
+        {/* <KakaoMap /> */}
+        <Image src={KakaoMapImage} alt="카카오맵 정적 이미지" />
+        <div className="flex flex-row justify-center items-center mt-2 mb-4">
+          <div className="flex mr-4">
+            <Image src={NaverMapIcon} alt="네이버맵 아이콘" height={24} width={24} className="mr-3" />
+            <a href="https://m.place.naver.com/place/1986598977/location?filter=location&selected_place_id=1986598977">네이버지도</a>
+          </div>
+          <div className="flex">
+            <Image src={KakaoMapIcon} alt="카카오맵 아이콘" height={24} width={24} className="mr-3" />
+            <a href="https://m.map.kakao.com/actions/detailMapView?id=8067878&refService=place">카카오지도</a>
+          </div>
+        </div>
       </section>
       <section className="location-info w-full flex flex-col justify-center items-start gap-2 pt-6 px-5 pb-10">
         <div className="location-info-section-text1 flex flex-col self-stretch font-extrabold text-[16px] leading-5 font-S-Core_Dream">
