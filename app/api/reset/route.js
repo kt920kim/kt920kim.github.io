ec66ca2d142d3aa4fc19f20ceffc4be4242af33f
Kitae Kim <kkt920@gmail.com>
2024-10-01 12:22:49 +0900

    add reset api handler to truncate guestbook json file

---
+++ app/api/reset/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
+
+let dataFilePath = '';
+if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
+  dataFilePath = path.join(process.cwd(), 'data', 'guestbook.json');
+} else {
+  dataFilePath = '/tmp/guestbook.json';
+}
+
+
+export async function GET() {
+  const fileContents = fs.truncateSync(dataFilePath);
+  return NextResponse.json('');
+}
